09b2c36e5523748f240bf1dddfb09f6275b65527
Oleksandr Khlopiachyi <akhlopiachyi@openware.com>
2021-05-31 20:22:36 +0300

    Enhancement: Remove dispatch of currenciesFetch, clean currencies module

--- web/src/containers/Wallets/History.tsx
+++ web/src/containers/Wallets/History.tsx
@@ -7,7 +7,6 @@ import { History, Pagination } from '../../components';
 import { Decimal } from '../../components/Decimal';
 import { localeDate } from '../../helpers';
 import {
-    currenciesFetch,
     Currency,
     fetchHistory,
     resetHistory,
@@ -44,7 +43,6 @@ export interface ReduxProps {
 }
 
 interface DispatchProps {
-    fetchCurrencies: typeof currenciesFetch;
     fetchHistory: typeof fetchHistory;
     resetHistory: typeof resetHistory;
 }
@@ -192,7 +190,6 @@ export const mapStateToProps = (state: RootState): ReduxProps => ({
 
 export const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> =
     dispatch => ({
-        fetchCurrencies: () => dispatch(currenciesFetch()),
         fetchHistory: params => dispatch(fetchHistory(params)),
         resetHistory: () => dispatch(resetHistory()),
     });
